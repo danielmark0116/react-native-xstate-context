517b478e7deac938a0107d680a5838e5e53c320c
Daniel Grychtol <grychtol.daniel@gmail.com>
2020-01-18 01:21:23 +0100

    scroll to refresh

--- App.tsx
+++ App.tsx
@@ -5,6 +5,11 @@ import {
   View,
   Text,
   TouchableOpacity,
+  ScrollView,
+  Dimensions,
+  ListView,
+  SectionList,
+  FlatList,
 } from 'react-native';
 
 import {EventObject} from 'xstate';
@@ -12,6 +17,11 @@ import {EventObject} from 'xstate';
 import {Machine, assign} from 'xstate';
 
 import {useMachine} from '@xstate/react';
+import {Button} from 'react-native-paper';
+
+const mockData = [...Array(40)].map((x, index) => `${index + 1}`);
+
+const dataPerFetch = 10;
 
 interface MachineContext {
   data: string[];
@@ -48,6 +58,10 @@ const myMachine = Machine<MachineContext, MachineStateSchema, Events>({
   },
   states: {
     idle: {
+      invoke: {
+        id: 'init-fetch',
+        src: () => cb => cb(EventTypes.FETCH),
+      },
       on: {
         [EventTypes.FETCH]: {
           target: 'fetching',
@@ -58,18 +72,25 @@ const myMachine = Machine<MachineContext, MachineStateSchema, Events>({
       on: {
         [EventTypes.MORE]: {
           target: 'more',
-          actions: (s: any, event: any) => console.log(s, event),
+          actions: assign({
+            data: ({data}, event: any) => [...data, ...event.newData],
+          }),
         },
       },
       invoke: {
-        src: (context: any, _e: any) => {
-          // console.log(context);
+        src: (context: MachineContext, _e: any) => {
+          const {data} = context;
 
           return async (cb: any, _onEvent: any) => {
-            await new Promise((res, rej) => setTimeout(rej, 1000));
+            await new Promise((res, rej) => setTimeout(res, 500));
+
+            const newData = mockData.slice(
+              data.length,
+              data.length + dataPerFetch,
+            );
 
             // this callback can be replaced with onDone
-            cb({type: EventTypes.MORE, toAdd: ['lasd']});
+            cb({type: EventTypes.MORE, newData});
           };
         },
         // if callback is used in invoke key then the onDone is skipped
@@ -78,7 +99,9 @@ const myMachine = Machine<MachineContext, MachineStateSchema, Events>({
       },
     },
     more: {
-      // onEntry: (cxt: any) => console.log(cxt),
+      on: {
+        [EventTypes.FETCH]: {target: 'fetching'},
+      },
     },
     complete: {type: 'final'},
     fail: {type: 'final'},
@@ -87,6 +110,8 @@ const myMachine = Machine<MachineContext, MachineStateSchema, Events>({
 
 const App = () => {
   const [curState, send] = useMachine(myMachine);
+
+  const data = curState.context.data;
 
   return (
     <SafeAreaView style={styles.center}>
@@ -97,13 +122,26 @@ const App = () => {
         }}>
         <Text>Start machine</Text>
       </TouchableOpacity>
-      <TouchableOpacity
+      <Button
         onPress={() => {
           send(EventTypes.DONE);
         }}>
         <Text>Complete machine</Text>
-      </TouchableOpacity>
-      <Text>{curState.value}</Text>
+      </Button>
+      <Text style={{marginBottom: 30}}>{curState.value}</Text>
+      <FlatList
+        data={data}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({item}) => (
+          <Text style={{height: 200, backgroundColor: 'grey', margin: 10}}>
+            {item}
+          </Text>
+        )}
+        onEndReachedThreshold={0.25}
+        onEndReached={() => send(EventTypes.FETCH)}
+        // keyExtractor={(item: any, index: any) => index}
+        // renderItem={({item}) => <Text>{item}</Text>}
+      />
     </SafeAreaView>
   );
 };
@@ -112,7 +150,6 @@ export default App;
 const styles = StyleSheet.create({
   center: {
     flex: 1,
-    alignItems: 'center',
   },
   title: {
     fontSize: 28,
